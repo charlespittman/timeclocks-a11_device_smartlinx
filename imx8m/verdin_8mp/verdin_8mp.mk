bc5908d6f6a4d5b7d51f836304bd152de546e5d0
Roberto Sartori <roberto.sartori@kynetics.com>
2022-03-14 11:15:15 +0000

    verdin_8mp: update to the 1.1 Bluetooth HAL version
    Needed to use the BTLinux HAL (for bluetooth over USB support).
    
    Signed-off-by: Roberto Sartori <roberto.sartori@kynetics.com>

--- imx8m/verdin_8mp/verdin_8mp.mk
+++ imx8m/verdin_8mp/verdin_8mp.mk
@@ -287,8 +287,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth HAL
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service
+    android.hardware.bluetooth@1.1-impl \
+    android.hardware.bluetooth@1.1-service.btlinux
 
 #nxp 8997 Bluetooth vendor config
 PRODUCT_PACKAGES += \
