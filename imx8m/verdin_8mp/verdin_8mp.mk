9edaa7d6d645293d364b2aecec6478b978dd33ed
Roberto Sartori <roberto.sartori@kynetics.com>
2022-11-25 14:56:35 +0000

    verdin_8mp: build and include Bluetooth HAL version 1.0
    Signed-off-by: Roberto Sartori <roberto.sartori@kynetics.com>

--- imx8m/verdin_8mp/verdin_8mp.mk
+++ imx8m/verdin_8mp/verdin_8mp.mk
@@ -287,8 +287,9 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth HAL
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.1-impl \
-    android.hardware.bluetooth@1.1-service.btlinux
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
+    android.hardware.bluetooth@1.0-service.btlinux
 
 #nxp 8997 Bluetooth vendor config
 PRODUCT_COPY_FILES += \
